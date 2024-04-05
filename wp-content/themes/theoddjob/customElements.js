4e8d6a000b04525a3669cdffbbf379c68b5a7cbf
Olga-steblyk <olga.steblyk@gmail.com>
2024-04-04 23:32:16 -0400

    JAVA SCRIPT

---
+++ wp-content/themes/theoddjob/customElements.js
@@ -0,0 +1,15 @@
+class CopyrightYear extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+    }
+}
+
+customElements.define("x-year", CopyrightYear);
+
+class TwoSidedMarket extends HTMLElement{
+	connectedCallback(){
+		this.inner = `<a herf="For-JobSeekers">For Job-seekers</a>&nbsp; <a herf="For-JobHavers">For Job-havers></a>`
+		
+	}
+}
+customElements.define(x-twosides, TwoSidedMarket)
